6c50b478f573bdf717fe03f7e4deb08d11586620
Akhil_Persistent <100367429+akhilpe@users.noreply.github.com>
2022-05-20 15:37:34 +0530

    variable files

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "ami" {
+
+}
+variable "subnet_id" {
+
+}
+variable "security_groups" {
+
+}
+variable "instance_type" {
+
+}
+variable "key_name" {
+
+}
+variable "availability_zone" {
+
+}
+
+variable "device_name" {
+
+}
